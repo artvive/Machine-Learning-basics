134e3edb50d61738d0c31580a9cce73e70a59dd6
Arthur Vive <avive@uchicago.edu>
2017-01-22 18:47:41 -0600

    Updated tests

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-# %% Linear Regression
-from LinearRegression import LinearRegression
 import scipy.stats as st
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import log_loss
+from sklearn.tree import DecisionTreeClassifier
+
+from LogisticRegression import LogisticRegression
+from DecisionTree import DecisionTree
+from LinearRegression import LinearRegression
+
+
+#Linear Regression
+print("LINEAR REGRESSION TESTS")
 
 temp = np.ones((2, 4))
 
@@ -27,16 +35,16 @@ clf.fit(X, y)
 
 y_pred = clf.predict(X)
 
-print(clf.coef)
-print(y_pred[1:10])
-print(y[1:10])
+print("Regression coefs: ", clf.coef)
+print("Predictions: ", y_pred[1:10])
+print("True values: ", y[1:10])
 
 # %% Logistic Regression
-from LogisticRegression import LogisticRegression
+print("\nLOGISTIC REGRESSION TESTS")
 N = 100
 
-X = np.linspace(0,100, num=N)
-y = np.concatenate([np.ones(N//2), np.zeros(N//2)])
+X = np.linspace(0, 100, num=N)
+y = np.concatenate([np.ones(N // 2), np.zeros(N // 2)])
 mean = np.mean(X, axis=0)
 std = np.std(X, axis=0)
 X = (X - mean) / std
@@ -45,18 +53,51 @@ clf = LogisticRegression()
 
 clf.fit(X, y)
 
-print(clf.predict((np.array([1, 2, 49, 50, 51, 100]) - mean) / std))
+test_x = np.array([1, 2, 49, 50, 51, 99])
 
-#mat = []
-#for i in np.linspace(-0.1,0.1):
-#    temp = []
-#    for j in np.linspace(-15.5,5):
-#        clf.coef_[0,0] = i
-#        clf.coef_[1,0] = j
-#        temp+=[clf.score(X,y)]
-#    mat += [temp]
-#
-#mat = np.array(mat)
-#
-#plt.matshow(mat)
-#plt.colorbar()
+print("Predicted values: ", clf.predict((test_x - mean) / std))
+print("True Values: ", y[test_x])
+
+# Decision Tree tests
+
+print("\nDECISION TREE TESTS")
+N = 100
+
+X = np.linspace(0, 100, num=N)
+X = X[:, np.newaxis]
+y = np.concatenate([np.ones(N // 2), np.zeros(N // 2)])
+clf = DecisionTree()
+clf.fit(X, y)
+print("(Accuracy, log loss)")
+print("Score on toy data set: ", clf.score(X, y))
+
+from sklearn.datasets import load_digits
+
+print("Mnist dataset:")
+df = load_digits()
+
+X = df['data']
+y = df['target']
+N = y.shape[0]
+np.random.seed(123)
+perm = np.random.permutation(N)
+
+n_test = N // 10
+X_test = X[perm[:n_test]]
+y_test = y[perm[:n_test]]
+
+X = X[perm[n_test:]]
+y = y[perm[n_test:]]
+
+clf = DecisionTree()
+clf.fit(X, y)
+
+print("Train score (our implementation): ", clf.score(X, y))
+print("Test score (our implementation): ", clf.score(X_test, y_test))
+
+clf2 = DecisionTreeClassifier(criterion="entropy")
+clf2.fit(X, y)
+print("Train score (sklearn): ", (clf2.score(X, y),
+      log_loss(y, clf2.predict_proba(X))))
+print("Train score (sklearn): ", (clf2.score(X_test, y_test),
+      log_loss(y_test, clf2.predict_proba(X_test))))
